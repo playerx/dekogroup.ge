6487dd32122ff0bb92541ce3f248b02ef6096e9c
Ezeki <ezeki@jok.ge>
2012-11-29 20:59:30 +0400

    stuff

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,25 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'index' });
+  res.render('index', { title: 'Deko Group' });
 };
 
 exports.work = function(req, res){
-  res.render('work', { title: 'work' });
+  res.render('work', { title: 'Deko Group - Work' });
 };
 
 exports.team = function(req, res){
-  res.render('team', { title: 'team' });
+  res.render('team', { title: 'Deko Group - Team' });
 };
 
 exports.partners = function(req, res){
-  res.render('partners', { title: 'partners' });
+  res.render('partners', { title: 'Deko Group - Partners' });
 };
 
 exports.links = function(req, res){
-  res.render('links', { title: 'links' });
+  res.render('links', { title: 'Deko Group - Links' });
 };
 
 exports.contact = function(req, res){
-  res.render('contact', { title: 'contact' });
+  res.render('contact', { title: 'Deko Group - Contact' });
 };
